fedd5e8057559f31af3786e077c3cf581d2cdc89
The Mai <Mai.The.09@gmail.com>
2018-12-27 03:05:27 -0600

    integrate position based transform

--- javascript/animation-zone.js
+++ javascript/animation-zone.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function (event) {
+	// Javascript for slice reveal effect on header
 	const sr_target = document.getElementById('header');
 	const sr_instance = sliceRevealer(sr_target, {
 		direction: 'vertical',
@@ -14,5 +15,40 @@ document.addEventListener("DOMContentLoaded", function (event) {
 		startCB: () => {
 			document.getElementById('header-cover').style.display = "none";
 		}
+	});
+
+	// Javascript for position based transform effect on header	
+	const pbt_header = document.getElementById('header');
+	const pbt_hero = document.getElementById('hero-header');
+	const pbt_logo = document.getElementById('hero-logo');
+	const pbt_title = document.getElementById('hero-title');
+	const pbt_subtitle = document.getElementById('hero-subtitle');
+	pBTransform(pbt_hero, {
+		hoverTarget: pbt_header,
+		translateY: 10,
+		rotate: 5,
+		scale: 1.2,
+		duration: "1s"
+	});
+	pBTransform(pbt_logo, {
+		hoverTarget: pbt_header,
+		translateX: 40,
+		tiltX: 30,		
+	});
+	pBTransform(pbt_title, {
+		hoverTarget: pbt_header,
+		translateX: 30,
+		rotate: 7,
+		rotateStyle: 2,
+		tiltX: 50,		
+		duration: ".8s"
+	});
+	pBTransform(pbt_subtitle, {
+		hoverTarget: pbt_header,
+		translateX: 20,
+		rotate: 8,
+		rotateStyle: 2,
+		tiltX: 70,		
+		duration: "1.4s"
 	});
 });
